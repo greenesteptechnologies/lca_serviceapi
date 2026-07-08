2afb34d386e8f03a262b9744d33687a6d3268ab3
greenesteptechnologies <greenestep@greenestep.com>
2026-07-08 19:24:40 +0530

    prod-changes

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 export const ENV = {
   PORT: process.env.PORT,
